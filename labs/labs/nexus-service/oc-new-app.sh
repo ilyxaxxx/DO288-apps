87b25673fa5099434f7f30feebb50f2199142745
Ilyxa <ilyxa.krylov@gmail.com>
2020-04-28 10:49:56 +0400

    ex1

--- labs/labs/nexus-service/oc-new-app.sh
+++ labs/labs/nexus-service/oc-new-app.sh
@@ -2,6 +2,6 @@
 
 source /usr/local/etc/ocp4.config
 
-oc new-app --name nexus3 --f ~/nexus-template.yaml \
+oc new-app --name nexus3 -f ~/nexus-template.yaml \
     -p HOSTNAME=nexus-${RHT_OCP4_DEV_USER}.${RHT_OCP4_WILDCARD_DOMAIN}
 
